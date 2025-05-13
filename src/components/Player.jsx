d946093d570f69cbb17aa76c5800d7cdac456f01
manavpanchotiya <manavpanchotiya@gmail.com>
2025-05-13 16:05:53 -0500

    conditional refactoring

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,14 +1,13 @@
-function Player(){
+function Player(props){
   return(
     <article className="Player">
-        <h1>John <span>Secret</span> Doe
+        <h1>{props.firstName} <span>{props.gamerTag}</span> {props.lastName}
         </h1>
-        {/* To be shown when there are no wins */}
-        <h2 className="zero">Currently with no wins</h2>
-        {/* To be shown when there is 1 win */}
-        <h2>Currently at 1 win</h2>
-        {/* To be shown when there is more than one win */}
-        <h2>Currently at 1+ wins</h2>
+        {props.wins === 0 && <h2 className="zero">Currently with no wins :p </h2>}
+        
+        {props.wins === 1 && <h2>Currently at 1 win</h2>}
+        
+        {props.wins > 1 && <h2>Currently at {props.wins} wins</h2>}
     </article>
   )
 }
--- src/components/PlayerList.jsx
+++ src/components/PlayerList.jsx
@@ -1,11 +1,19 @@
-import Player from "./Player"
+import Player from "./Player";
+import playerData from "../data/playerData";
+import matchData from "../data/matchData";
+import { preparePlayerData, addWinsToPlayers } from "../helpers/playerHelpers";
 
-function PlayerList(){
+function PlayerList(props){
+  const playerDataArray = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+  const onePlayer = parsedPlayerData[0];
+  // console.log(parsedPlayerData);
   return(
 
     <section className="PlayerList">
       <h1>Current participating players</h1>
-      <Player/>    
+      <Player gamerTag={onePlayer.gamerTag} firstName={onePlayer.firstName} lastName={onePlayer.lastName} wins={onePlayer.wins}/>
+            
     </section>
   )  
 }
