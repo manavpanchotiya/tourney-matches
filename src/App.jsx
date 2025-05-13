7dff295438f5828dd119ae36772e5bc1e570a7e2
manavpanchotiya <manavpanchotiya@gmail.com>
2025-05-13 09:24:56 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import MatchList from './components/MatchList'
+import PlayerList from './components/PlayerList'
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>
+        Tourney Matches{""} <span>Where Coding and Tournaments found their Match!</span>
+      </h1>
+      <PlayerList/>
+      <MatchList/>
+
+    </div>
+    
+  );
+}
+
+export default App
